6882d05e56ace4a20acef9cad8b02746ed6a9929
LEONID <leonid.gromov.2016@mail.ru>
2026-04-03 23:19:55 +0300

    APP

--- conftest.py
+++ conftest.py
@@ -1,13 +1,28 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(autouse=True)
 def setup_browser():
-    browser.config.base_url = 'https://demoqa.com'
+    options = Options()
+
+    options.set_capability("browserName", "chrome")
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": False
+    })
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
     yield
 
-    browser.quit()
+    driver.quit()
